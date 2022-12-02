9c5103efe21bb03164884c01e087b48ab5713ee5
fiskryeziu <fiskryeziu@gmail.com>
2022-12-02 22:38:26 +0100

    2 day puzzle two

--- 2/b.js
+++ 2/b.js
@@ -2,20 +2,10 @@ const fs = require('fs')
 
 const data = fs.readFileSync('./input.txt', 'utf8')
 let result
-// const arr = data.split(/\r?\n/)
-// const array = arr.map((item) => item.replace(' ', ''))
-const array = ['AY', 'BX', 'CZ']
-// console.log(arr)
-// 1 for Rock
-// 2 for Paper
-// 3 for Scissors
-// 0 if you lost
-// 3 if the round was a draw
-// and 6 if you won).
-// oponent A rock B paper C scissors
-// me X rock  Y paper Z Scissors
+const arr = data.split(/\r?\n/)
+const array = arr.map((item) => item.replace(' ', ''))
+// const array = ['AY', 'BX', 'CZ']
 
-//check through rounds
 function sumAllValues(array) {
   let valueOfRounds = []
   for (let i = 0; i < array.length; i++) {
@@ -24,7 +14,6 @@ function sumAllValues(array) {
   return valueOfRounds.reduce((acc, item) => acc + item, 0)
 }
 
-//exit
 function getArray(i, valueOfRounds) {
   let opponent = i[0]
   let user = i[1]
@@ -33,34 +22,34 @@ function getArray(i, valueOfRounds) {
   let drawValue = 0
   let value = 0
   let total
-  if (i === 'AX' || i === 'BY' || i === 'CZ') {
-    drawValue = 3
-    if (user === 'X') {
+  if (user === 'X') {
+    if (opponent === 'B') {
       value += 1
-    } else if (user === 'Y') {
+    } else if (opponent === 'C') {
       value += 2
     } else {
       value += 3
-    }
-  } else if (user === 'X') {
-    if (opponent === 'B') {
-      value += 1
-    } else {
-      value += 1
-      winValue += 6
     }
   } else if (user === 'Y') {
     if (opponent === 'C') {
-      value += 2
+      value += 3
+      drawValue += 3
+    } else if (opponent === 'A') {
+      value += 1
+      drawValue += 3
     } else {
       value += 2
-      winValue += 6
+      drawValue += 3
     }
   } else if (user === 'Z') {
     if (opponent === 'A') {
+      value += 2
+      winValue += 6
+    } else if (opponent === 'B') {
       value += 3
+      winValue += 6
     } else {
-      value += 3
+      value += 1
       winValue += 6
     }
   }
